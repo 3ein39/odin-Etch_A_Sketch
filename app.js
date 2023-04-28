cfd1ba6a29f94c10517219855f680c5fdf81eae4
HusseinHany <hhany3997@gmail.com>
2023-04-28 22:56:43 +0200

    implement hovering effect

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ for (let i = 0; i < 16*16; ++i) {
     let child = cont.appendChild(document.createElement('div'));
     child.classList.add('square');
 }
+
+let grid = cont.querySelectorAll('div');
+
+grid.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+        if (!square.classList.contains('blur'))
+            square.classList.add('blur');
+    })
+})
