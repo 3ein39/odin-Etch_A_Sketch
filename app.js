9373f9f49ab1d8ec3646b830422aa41e39a69317
HusseinHany <hhany3997@gmail.com>
2023-04-28 22:47:20 +0200

    implement grid divs

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("hello")
+let cont = document.querySelector('.container');
+
+for (let i = 0; i < 16*16; ++i) {
+    let child = cont.appendChild(document.createElement('div'));
+    child.classList.add('square');
+}
